7d050a51ba38798cdee9e35991d35032c517b8b9
Judah Schvimer <judah@mongodb.com>
2015-12-11 14:57:10 -0500

    fixed mongoimport post master session change

--- mongoimport/mongoimport.go
+++ mongoimport/mongoimport.go
@@ -395,7 +395,6 @@ func (imp *MongoImport) ingestDocuments(readDocs chan bson.D) (retErr error) {
 // 1. Sets the session to not timeout
 // 2. Sets the write concern on the session
 // 3. Sets the session safety
-// 4. Sets bypass document validation if requested
 //
 // returns an error if it's unable to set the write concern
 func (imp *MongoImport) configureSession(session *mgo.Session) error {
@@ -406,10 +405,6 @@ func (imp *MongoImport) configureSession(session *mgo.Session) error {
 		return fmt.Errorf("write concern error: %v", err)
 	}
 	session.SetSafe(sessionSafety)
-
-	if imp.IngestOptions.BypassDocumentValidation {
-		session.SetBypassValidation(true)
-	}
 
 	return nil
 }
